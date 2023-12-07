f1db19640d6f419b213cd94aa2826d0d1e94a793
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-07 18:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function solve(y) {
 	if((y % 400 == 0) ||
        (y % 100 != 0) && 
-       (y % 4 == 0)))return 366;
+       (y % 4 == 0))return 366;
 	return 365;
 }
 const year=prompt("Enter the year : ");
